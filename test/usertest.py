2c3ccce887bb7b8dee17f17e9a728f3a4fda12cb
avoliva <adamvoliva@gmail.com>
2012-08-16 11:11:00 -0700

    Unit Testing - Fixed test json, all on one line. JSONs match now.

--- test/usertest.py
+++ test/usertest.py
@@ -1,4 +1,7 @@
 from scim import user
+from scim import core
+from scim import group
+from scim import attributes
 import unittest
 import json
 
@@ -15,7 +18,6 @@ class JSONTestCase(unittest.TestCase):
                 middle="Jane",
                 prefix="Ms.",
                 suffix="III"
-                
             ),
         )
         self.u.username = "bjensen@example.com"
@@ -42,7 +44,7 @@ class JSONTestCase(unittest.TestCase):
                 region='CA',
                 code='91608',
                 country='USA',
-                fomrated='100 Universal City Plaza\nHollywood, CA 91608 USA'
+                fomrated='100 Universal City Plaza Hollywood, CA 91608 USA'
             ),
             user.User.Address(
                 type='home',
@@ -51,7 +53,7 @@ class JSONTestCase(unittest.TestCase):
                 region='CA',
                 code='91608',
                 country='USA',
-                formatted="456 Hollywood Blvd\nHollywood, CA 91608 USA"
+                formatted="456 Hollywood Blvd Hollywood, CA 91608 USA"
             )
         ]
         self.u.PhoneNumber = [
@@ -87,7 +89,7 @@ class JSONTestCase(unittest.TestCase):
         self.u.timezone='America/Los_Angeles'
         
         self.u.password='t1meMa$heen'
-        self.u.Group = [
+        self.u.groups = [
             user.User.Group(
                 display='Tour Guides',
                 value='00300000005N2Y6AA'
@@ -101,155 +103,28 @@ class JSONTestCase(unittest.TestCase):
                 value='00300000005N98YT1'
             )
         ]
-        self.u.Certificate = [
+        self.u.certs = [
             user.User.Certificate(
-                value="""MIIDQzCCAqygAwIBAgICEAAwDQYJKoZIhvcNAQEFBQAwTjELMAkGA1UEBhMCVVMx
-                EzARBgNVBAgMCkNhbGlmb3JuaWExFDASBgNVBAoMC2V4YW1wbGUuY29tMRQwEgYD
-                VQQDDAtleGFtcGxlLmNvbTAeFw0xMTEwMjIwNjI0MzFaFw0xMjEwMDQwNjI0MzFa
-                MH8xCzAJBgNVBAYTAlVTMRMwEQYDVQQIDApDYWxpZm9ybmlhMRQwEgYDVQQKDAtl
-                eGFtcGxlLmNvbTEhMB8GA1UEAwwYTXMuIEJhcmJhcmEgSiBKZW5zZW4gSUlJMSIw
-                IAYJKoZIhvcNAQkBFhNiamVuc2VuQGV4YW1wbGUuY29tMIIBIjANBgkqhkiG9w0B
-                AQEFAAOCAQ8AMIIBCgKCAQEA7Kr+Dcds/JQ5GwejJFcBIP682X3xpjis56AK02bc
-                1FLgzdLI8auoR+cC9/Vrh5t66HkQIOdA4unHh0AaZ4xL5PhVbXIPMB5vAPKpzz5i
-                PSi8xO8SL7I7SDhcBVJhqVqr3HgllEG6UClDdHO7nkLuwXq8HcISKkbT5WFTVfFZ
-                zidPl8HZ7DhXkZIRtJwBweq4bvm3hM1Os7UQH05ZS6cVDgweKNwdLLrT51ikSQG3
-                DYrl+ft781UQRIqxgwqCfXEuDiinPh0kkvIi5jivVu1Z9QiwlYEdRbLJ4zJQBmDr
-                SGTMYn4lRc2HgHO4DqB/bnMVorHB0CC6AV1QoFK4GPe1LwIDAQABo3sweTAJBgNV
-                HRMEAjAAMCwGCWCGSAGG+EIBDQQfFh1PcGVuU1NMIEdlbmVyYXRlZCBDZXJ0aWZp
-                Y2F0ZTAdBgNVHQ4EFgQU8pD0U0vsZIsaA16lL8En8bx0F/gwHwYDVR0jBBgwFoAU
-                dGeKitcaF7gnzsNwDx708kqaVt0wDQYJKoZIhvcNAQEFBQADgYEAA81SsFnOdYJt
-                Ng5Tcq+/ByEDrBgnusx0jloUhByPMEVkoMZ3J7j1ZgI8rAbOkNngX8+pKfTiDz1R
-                C4+dx8oU6Za+4NJXUjlL5CvV6BEYb1+QAEJwitTVvxB/A67g42/vzgAtoRUeDov1
-                +GFiBZ+GNF/cAYKcMtGcrs2i97ZkJMo="""
+                value="""333"""
             )
         ]
-        self.testJson = """{
-  "schemas": ["urn:scim:schemas:core:1.0"],
-  "id": "2819c223-7f76-453a-919d-413861904646",
-  "externalId": "701984",
-  "userName": "bjensen@example.com",
-  "name": {
-    "formatted": "Ms. Barbara J Jensen III",
-    "familyName": "Jensen",
-    "givenName": "Barbara",
-    "middleName": "Jane",
-    "honorificPrefix": "Ms.",
-    "honorificSuffix": "III"
-  },
-  "displayName": "Babs Jensen",
-  "nickName": "Babs",
-  "profileUrl": "https://login.example.com/bjensen",
-  "emails": [
-    {
-      "value": "bjensen@example.com",
-      "type": "work",
-      "primary": true
-    },
-    {
-      "value": "babs@jensen.org",
-      "type": "home"
-    }
-  ],
-  "addresses": [
-    {
-      "type": "work",
-      "streetAddress": "100 Universal City Plaza",
-      "locality": "Hollywood",
-      "region": "CA",
-      "postalCode": "91608",
-      "country": "USA",
-      "formatted": "100 Universal City Plaza\nHollywood, CA 91608 USA",
-      "primary": true
-    },
-    {
-      "type": "home",
-      "streetAddress": "456 Hollywood Blvd",
-      "locality": "Hollywood",
-      "region": "CA",
-      "postalCode": "91608",
-      "country": "USA",
-      "formatted": "456 Hollywood Blvd\nHollywood, CA 91608 USA"
-    }
-  ],
-  "phoneNumbers": [
-    {
-      "value": "555-555-5555",
-      "type": "work"
-    },
-    {
-      "value": "555-555-4444",
-      "type": "mobile"
-    }
-  ],
-  "ims": [
-    {
-      "value": "someaimhandle",
-      "type": "aim"
-    }
-  ],
-  "photos": [
-    {
-      "value": "https://photos.example.com/profilephoto/72930000000Ccne/F",
-      "type": "photo"
-    },
-    {
-      "value": "https://photos.example.com/profilephoto/72930000000Ccne/T",
-      "type": "thumbnail"
-    }
-  ],
-  "userType": "Employee",
-  "title": "Tour Guide",
-  "preferredLanguage":"en_US",
-  "locale": "en_US",
-  "timezone": "America/Los_Angeles",
-  "active":true,
-  "password":"t1meMa$heen",
-  "groups": [
-    {
-      "display": "Tour Guides",
-      "value": "00300000005N2Y6AA"
-    },
-    {
-      "display": "Employees",
-      "value": "00300000005N34H78"
-    },
-    {
-      "display": "US Employees",
-      "value": "00300000005N98YT1"
-    }
-  ],
-  "x509Certificates": [
-    {
-      "value": "MIIDQzCCAqygAwIBAgICEAAwDQYJKoZIhvcNAQEFBQAwTjELMAkGA1UEBhMCVVMx
-                EzARBgNVBAgMCkNhbGlmb3JuaWExFDASBgNVBAoMC2V4YW1wbGUuY29tMRQwEgYD
-                VQQDDAtleGFtcGxlLmNvbTAeFw0xMTEwMjIwNjI0MzFaFw0xMjEwMDQwNjI0MzFa
-                MH8xCzAJBgNVBAYTAlVTMRMwEQYDVQQIDApDYWxpZm9ybmlhMRQwEgYDVQQKDAtl
-                eGFtcGxlLmNvbTEhMB8GA1UEAwwYTXMuIEJhcmJhcmEgSiBKZW5zZW4gSUlJMSIw
-                IAYJKoZIhvcNAQkBFhNiamVuc2VuQGV4YW1wbGUuY29tMIIBIjANBgkqhkiG9w0B
-                AQEFAAOCAQ8AMIIBCgKCAQEA7Kr+Dcds/JQ5GwejJFcBIP682X3xpjis56AK02bc
-                1FLgzdLI8auoR+cC9/Vrh5t66HkQIOdA4unHh0AaZ4xL5PhVbXIPMB5vAPKpzz5i
-                PSi8xO8SL7I7SDhcBVJhqVqr3HgllEG6UClDdHO7nkLuwXq8HcISKkbT5WFTVfFZ
-                zidPl8HZ7DhXkZIRtJwBweq4bvm3hM1Os7UQH05ZS6cVDgweKNwdLLrT51ikSQG3
-                DYrl+ft781UQRIqxgwqCfXEuDiinPh0kkvIi5jivVu1Z9QiwlYEdRbLJ4zJQBmDr
-                SGTMYn4lRc2HgHO4DqB/bnMVorHB0CC6AV1QoFK4GPe1LwIDAQABo3sweTAJBgNV
-                HRMEAjAAMCwGCWCGSAGG+EIBDQQfFh1PcGVuU1NMIEdlbmVyYXRlZCBDZXJ0aWZp
-                Y2F0ZTAdBgNVHQ4EFgQU8pD0U0vsZIsaA16lL8En8bx0F/gwHwYDVR0jBBgwFoAU
-                dGeKitcaF7gnzsNwDx708kqaVt0wDQYJKoZIhvcNAQEFBQADgYEAA81SsFnOdYJt
-                Ng5Tcq+/ByEDrBgnusx0jloUhByPMEVkoMZ3J7j1ZgI8rAbOkNngX8+pKfTiDz1R
-                C4+dx8oU6Za+4NJXUjlL5CvV6BEYb1+QAEJwitTVvxB/A67g42/vzgAtoRUeDov1
-                +GFiBZ+GNF/cAYKcMtGcrs2i97ZkJMo="
-    }
-  ],
-  "meta": {
-    "created": "2010-01-23T04:56:22Z",
-    "lastModified": "2011-05-13T04:42:34Z",
-    "version": "W\/\"a330bc54f0671c9\"",
-    "location": "https://example.com/v1/Users/2819c223-7f76-453a-919d-413861904646"
-  }
-}"""
+        self.u.meta= (
+            core.Core.Metadata(
+                created="2010-01-23T04:56:22Z",
+                modified="2011-05-13T04:42:34Z",
+                version="1",
+                location="https://example.com/v1/Users/2819c223-7f76-453a-919d-413861904646"
+            )
+        )
+        self.u.id="2819c223-7f76-453a-919d-413861904646"
+        self.u.external_id="701984"
+        self.testJson = """{"userName": "bjensen@example.com", "displayName": "Babs Jensen", "addresses": [{"type": "work", "primary": true, "value": {"postalCode": "91608", "country": "USA", "region": "CA", "streetAddress": "100 Universal City Plaza", "locality": "Hollywood"}}, {"type": "home", "value": {"locality": "Hollywood", "country": "USA", "region": "CA", "formatted": "456 Hollywood Blvd Hollywood, CA 91608 USA", "streetAddress": "456 Hollywood Blvd", "postalCode": "91608"}}], "title": "Tour Guide", "x509Certificates": [{"value": "333"}], "schemas": ["urn:scim:schemas:core:1.0"], "id": "2819c223-7f76-453a-919d-413861904646", "profileUrl": "https://login.example.com/bjensen", "preferredLanguage": "en_US", "meta": {"lastModified": "2011-05-13T04:42:34Z", "version": "1", "location": "https://example.com/v1/Users/2819c223-7f76-453a-919d-413861904646", "created": "2010-01-23T04:56:22Z"}, "externalId": "701984", "groups": [{"display": "Tour Guides", "value": "00300000005N2Y6AA"}, {"display": "Employees", "value": "00300000005N34H78"}, {"display": "US Employees", "value": "00300000005N98YT1"}], "timezone": "America/Los_Angeles", "locale": "en_US", "password": "t1meMa$heen", "nickName": "Babs", "emails": [{"type": "work", "primary": true, "value": "bejensen@example.com"}, {"type": "home", "value": "babs@jensen.org"}], "userType": "Employee", "name": {"honorificPrefix": "Ms.", "middleName": "Jane", "familyName": "Jensen", "formatted": "Ms. Barbara J Jensen III", "givenName": "Barbara", "honorificSuffix": "III"}}"""
 
     def runTest(self):
         
-        self.userJson = json.dumps(user.User.serialize(self.u), indent=2)
+        self.userJson = json.dumps(user.User.serialize(self.u))
         print self.userJson
+        
+        #deJson = json.dumps(json.loads(self.testJson))
+        
         self.assertEqual(self.userJson, self.testJson,"JSON's don't match")
