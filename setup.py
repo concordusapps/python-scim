ca768b7af5d8a86feec864529b780be630f91ab8
Ryan Leckey <rleckey@concordusapps.com>
2013-07-05 16:22:46 -0700

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,15 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+from setuptools import setup, find_packages
+
+setup(
+    name='scim',
+    version='0.2.0',
+    description='A python interface to produce and consume System for Cross-domain Identity Management (SCIM) messages.',
+    author='Concordus Applications',
+    author_email='support@concordusapps.com',
+    url='http://github.com/concordusapps/python-scim',
+    package_dir={'scim': 'src/scim'},
+    packages=find_packages('src'),
+    install_requires=(),
+)
